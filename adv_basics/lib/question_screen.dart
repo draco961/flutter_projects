8144f09ce86434c7affeceed670aba998de1aeca
draco961 <email@example.com>
2026-03-12 10:16:49 +0100

    75. Managing The Questions Index As State

--- adv_basics/lib/question_screen.dart
+++ adv_basics/lib/question_screen.dart
@@ -10,23 +10,40 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
 
-  // void Function() onTap() {}
+  void onTap() {}
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
 
   @override
   Widget build(context) {
-    var currentQuestion = questions[0];
+    var currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        spacing: 10,
-        children: [
-          Text(currentQuestion.text, style: TextStyle(color: Colors.white)),
-          SizedBox(height: 20),
-          ...currentQuestion.answers.map((a) => AnswerButton(answerText: a, onTap: () {})) ,
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          spacing: 10,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            ...currentQuestion.getShuffledAnswers().map(
+              (a) => AnswerButton(answerText: a, onTap: answerQuestion),
+            ),
+          ],
+        ),
       ),
     );
   }
