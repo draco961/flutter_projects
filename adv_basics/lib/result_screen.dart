64cf10eb58281cfa6ace9eba71563dec4b99f180
draco961 <email@example.com>
2026-03-12 14:53:41 +0100

    93. The starting project And A problem

--- adv_basics/lib/result_screen.dart
+++ adv_basics/lib/result_screen.dart
@@ -1,6 +1,6 @@
 import 'package:adv_basics/answer_button.dart';
 import 'package:adv_basics/data/questions.dart';
-import 'package:adv_basics/question_summary.dart';
+import 'package:adv_basics/questions_summary/question_summary.dart';
 import 'package:flutter/material.dart';
 
 class ResultScreen extends StatelessWidget {
