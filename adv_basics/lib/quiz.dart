f1d2a3ff5658fc3b05e3030f57414886a445c80f
draco961 <email@example.com>
2026-03-12 13:43:55 +0100

    86. Combining Columns & Rows

--- adv_basics/lib/quiz.dart
+++ adv_basics/lib/quiz.dart
@@ -1,5 +1,6 @@
 import 'package:adv_basics/data/questions.dart';
 import 'package:adv_basics/question_screen.dart';
+import 'package:adv_basics/result_screen.dart';
 import 'package:adv_basics/start_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -12,7 +13,7 @@ class Quiz extends StatefulWidget {
 
 class _QuizState extends State<Quiz> {
   var activeScreen = 'start-screen';
-  final List<String> selectedAnswers = [];
+  List<String> selectedAnswers = [];
 
   @override
   void initState() {
@@ -25,14 +26,27 @@ class _QuizState extends State<Quiz> {
     });
   }
 
-  void chooseAnswer(String answer){
+  void chooseAnswer(String answer) {
     print('answer : $answer');
-
     selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      // display result screen
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget currentScreen = StartScreen(switchScreen);
+
+    if (activeScreen == 'question-screen')
+      currentScreen = QuestionsScreen(onSelectedChooseAnswer: chooseAnswer);
+
+    if (activeScreen == 'results-screen') currentScreen = ResultScreen(chosenAnswers: [...selectedAnswers],);
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -43,9 +57,7 @@ class _QuizState extends State<Quiz> {
               colors: [Colors.deepPurple, Colors.deepPurpleAccent],
             ),
           ),
-          child: activeScreen == 'start-screen'
-              ? StartScreen(switchScreen)
-              : QuestionsScreen(onSelectedChooseAnswer: chooseAnswer),
+          child: currentScreen,
         ),
       ),
     );
---
+++ adv_basics/lib/result_screen.dart
@@ -0,0 +1,49 @@
+import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/question_summary.dart';
+import 'package:flutter/material.dart';
+
+class ResultScreen extends StatelessWidget {
+  final List<String> chosenAnswers;
+
+  const ResultScreen({super.key, required this.chosenAnswers});
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+    // for (var chosenAnswer in chosenAnswers) {
+    //   summary.add({ 'question_index': 1,  chosenAnswer});
+    // }
+
+    return summary;
+  }
+
+  @override
+  Widget build(context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('You answered X out of Y questiosn correctly!'),
+            const SizedBox(height: 30),
+            //const Text('List of answers and questions ...'),
+            QuestionSummary(getSummaryData()),
+            const SizedBox(height: 30),
+            TextButton(onPressed: () {}, child: Text('Restart Quid!')),
+          ],
+        ),
+      ),
+    );
+  }
+}
